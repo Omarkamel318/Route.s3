d17d5fb132dd09307ca6168c0173f13ec65b1d24
Omar Kamel <omarkamel318@gmail.com>
2024-03-20 14:59:38 +0200

    DAL

---
+++ Route.s3.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.s3.DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-U2HDASP\\MSSQLSERVER01; Database= MVCP1; trusted_Connection = True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Route.s3.DAL/Data/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Route.s3.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.s3.DAL.Data.Configurations
+{
+    internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(d => d.Id).UseIdentityColumn(10, 10);
+            builder.Property(d=>d.Name).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+                
+        }
+    }
+}
